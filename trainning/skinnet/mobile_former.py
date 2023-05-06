7dd0669931645af55d983a135c28fa4f5b0dde81
cuong3004 <duongduccuong30042002@gmail.com>
2023-05-06 16:29:30 +0700

    update

--- trainning/skinnet/mobile_former.py
+++ trainning/skinnet/mobile_former.py
@@ -13,10 +13,8 @@ import torch.nn.functional as F
 import math
 
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
-# from .helpers import build_model_with_cfg, default_cfg_for_features
-# import timm.models.
-from timm.models import build_model_with_cfg, register_model
-# from .registry import register_model
+from .helpers import build_model_with_cfg, default_cfg_for_features
+from .registry import register_model
 from .dna_blocks import DnaBlock, DnaBlock3, _make_divisible, MergeClassifier, Local2Global
 
 __all__ = ['MobileFormer']
@@ -229,7 +227,7 @@ def _create_mobile_former(variant, pretrained=False, **kwargs):
         MobileFormer, 
         variant, 
         pretrained,
-        default_cfg=default_cfgs['default'],
+        # default_cfg=default_cfgs['default'],
         **kwargs)
     print(model)
 
